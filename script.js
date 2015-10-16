8a97e886021dfb6f4cad6de6877a81508bfde09b
August <augustt198@gmail.com>
2015-10-15 22:43:11 -0400

    Make betters

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 var SIZE = 500;
+var GRID_SIZE = 20;
+
 var CTX = document.getElementById("draw").getContext("2d");
+
+var STATIONED = 0;
 
 /*
 {
@@ -22,7 +26,24 @@ var CTX = document.getElementById("draw").getContext("2d");
 }
 */
 
-CTX.fillStyle = "black";
+var VERTICES = [
+    [0, 0, 1, 1],
+    [GRID_SIZE, 0, -1, 1],
+    [0, GRID_SIZE, 1, 1],
+    [GRID_SIZE, GRID_SIZE, -1, 1]
+];
+
+var RECT_STACK = [];
+
+function redrawRects() {
+    CTX.fillStyle = "gray";
+    RECT_STACK.forEach(function (rect) {
+        CTX.fillRect(rect[0], rect[1], rect[2], rect[3]);
+    });
+}
+
+
+CTX.fillStyle = "white";
 CTX.fillRect(0, 0, SIZE, SIZE);
 
 // direction faux enum
@@ -33,7 +54,6 @@ var DIR = Object.freeze({
     LEFT: 3
 });
 
-var GRID_SIZE = 20;
 var GRID = new Array(GRID_SIZE);
 for (var i = 0; i < GRID.length; i++)
     GRID[i] = new Array(GRID_SIZE);
@@ -57,6 +77,7 @@ function betterRect(x, y, w, h) {
 
     CTX.fillStyle = "gray";
     CTX.fillRect(x, y, w, h);
+    RECT_STACK.push([x, y, w, h]);
 }
 
 
@@ -125,10 +146,13 @@ function validateRect(x, y, newX, newY, dir) {
         startY -= 2;
     }
 
+    if (startX < 0 || startY < 0) return false;
+
     // CTX.fillStyle = "rgba(0, 0, 200, 0.25)";
     // CTX.fillRect(startX * 25, startY * 25, (endX - startX) * 25, (endY - startY) * 25);
 
     for (var i = startX; i <= endX; i++) {
+        console.log("i = ", i);
         for (var j = startY; j <= endY; j++) {
             if (GRID[i][j] !== undefined) {
                 console.log("nope", x, y, newX, newY);
@@ -159,21 +183,24 @@ function genWall(x, y, prevDir, prb) {
 
     var dir;
     if (prevDir === undefined) {
-        if (y == 0)
+        if (y == 0) {
             dir = DIR.DOWN;
-        else if (y == GRID_SIZE - 1)
+        }
+        else if (y == GRID_SIZE - 1) {
             dir = DIR.UP;
-        else if (x == 0)
+        }
+        else if (x == 0) {
             dir = DIR.RIGHT;
-        else if (x == GRID_SIZE - 1)
+        }
+        else if (x == GRID_SIZE - 1) {
             dir = DIR.LEFT;
+        }
     } else {
         dir = randomDir(x, y, prevDir);
         if (dir === undefined)
             return;
     }
 
-    console.log("le dir", dir);
     var newX = x, newY = y;
     var rect;
     if (dir == DIR.UP) {
@@ -207,6 +234,26 @@ function genWall(x, y, prevDir, prb) {
         JOINTS.push([newX, newY]);
     }
 
+    if (prevDir === undefined) {
+        if (y == 0) {
+            VERTICES.push([x, y, -1, -1]);
+            VERTICES.push([x + 1, y, 1, -1]);
+        }
+        else if (y == GRID_SIZE - 1) {
+            VERTICES.push([x, y + 1, -1, 1]);
+            VERTICES.push([x + 1, y + 1, 1, 1]);
+        }
+        else if (x == 0) {
+            VERTICES.push([x, y, 1, -1]);
+            VERTICES.push([x, y + 1, 1, 1]);
+        }
+        else if (dir == DIR.LEFT) {
+            console.log("IS LEFT");
+            VERTICES.push([x, y, -1, -1]);
+            VERTICES.push([x, y + 1, -1, 1]);
+        }
+    }
+
     genWall(newX, newY, dir, prb / 1.2);
 }
 
@@ -225,7 +272,7 @@ function drawGrid() {
     }
 }
 
-//drawGrid();
+drawGrid();
 
 function seedWalk() {
     return 3 + Math.floor(Math.random() * (GRID_SIZE - 6));
@@ -242,38 +289,49 @@ for (var i = 0; i <= 5; i++) {
     }
 }
 
-var VERTICES = [];
 for (var i = 0; i < JOINTS.length; i++) {
     var x = JOINTS[i][0], y = JOINTS[i][1];
 
     // outer
     if (GRID[x - 1][y] === undefined && GRID[x - 1][y - 1] === undefined && GRID[x][y - 1] === undefined) {
-        VERTICES.push([x, y]);
+        VERTICES.push([x, y, -1, -1]);
     }
     if (GRID[x + 1][y] === undefined && GRID[x + 1][y - 1] === undefined && GRID[x][y - 1] === undefined) {
-        VERTICES.push([x + 1, y]);
+        VERTICES.push([x + 1, y, 1, -1]);
     }
     if (GRID[x - 1][y] === undefined && GRID[x - 1][y + 1] === undefined && GRID[x][y + 1] === undefined) {
-        VERTICES.push([x, y + 1]);
+        VERTICES.push([x, y + 1, -1, 1]);
     }
     if (GRID[x + 1][y] === undefined && GRID[x + 1][y + 1] === undefined && GRID[x][y + 1] === undefined) {
-        VERTICES.push([x + 1, y + 1]);
+        VERTICES.push([x + 1, y + 1, 1, 1]);
     }
 
     // inner
     if (GRID[x - 1][y] !== undefined && GRID[x - 1][y - 1] === undefined && GRID[x][y - 1] !== undefined) {
-        VERTICES.push([x, y]);
+        VERTICES.push([x, y, -1, -1]);
     }
     if (GRID[x + 1][y] !== undefined && GRID[x + 1][y - 1] === undefined && GRID[x][y - 1] !== undefined) {
-        VERTICES.push([x + 1, y]);
+        VERTICES.push([x + 1, y, 1, -1]);
     }
     if (GRID[x - 1][y] !== undefined && GRID[x - 1][y + 1] === undefined && GRID[x][y + 1] !== undefined) {
-        VERTICES.push([x, y + 1]);
+        VERTICES.push([x, y + 1, -1, 1]);
     }
     if (GRID[x + 1][y] !== undefined && GRID[x + 1][y + 1] === undefined && GRID[x][y + 1] !== undefined) {
-        VERTICES.push([x + 1, y + 1]);
+        VERTICES.push([x + 1, y + 1, 1, 1]);
     }
 }
+
+function drawVertices() {
+    VERTICES.forEach(function(v) {
+        CTX.fillStyle = "black";
+        CTX.beginPath();
+        CTX.arc(v[0] * 25, v[1] * 25, 5, 0, 2 * Math.PI);
+        CTX.fill();
+    });
+}
+
+drawVertices();
+redrawRects();
 
 CTX.fillStyle = "blue";
 /*
@@ -287,7 +345,7 @@ for (var i = 0; i < VERTICES.length; i++) {
 }
 */
 
-var colors = ["yellow", "green", "red"]
+var colors = ["yellow", "lime", "pink", "cyan", "lightsalmon"];
 var color_idx = 0;
 
 var elem = document.getElementById("draw");
@@ -296,25 +354,27 @@ elem.addEventListener('click', function (e) {
     console.log("Clicked at", cx, cy);
     VERTICES.forEach(function (v) {
         var x = v[0] * 25, y = v[1] * 25;
-        if (Math.pow(x - cx, 2) + Math.pow(y - cy, 2) <= 25) {
+        if (Math.pow(x - cx, 2) + Math.pow(y - cy, 2) <= 50) {
             console.log("Matches vertex!");
-            highlight(v[0], v[1]);
+            highlight(v[0], v[1], v[2], v[3]);
+            STATIONED++;
+            document.getElementById("feedback-used").innerText = STATIONED;
         }
     });
 });
 
-function highlight(x, y) {
-    var color = colors[color_idx++ % 3];
+function highlight(x, y, oX, oY) {
+    var color = colors[color_idx++ % colors.length];
     CTX.strokeStyle = color;
 
     var data = CTX.getImageData(0, 0, SIZE, SIZE).data;
 
     var xOff = x * 25, yOff = y * 25;
 
-    var epsilon = 0.002;
-    for (var d = -Math.PI / 2 + epsilon; d <= Math.PI / 2 - epsilon; d += 0.002) {
+    var epsilon = 0.001;
+    for (var d = -Math.PI / 2 + epsilon; d <= Math.PI / 2 - epsilon; d += 0.0015) {
         m = Math.tan(d);
-        var xi = 0, yi = 0;
+        var xi = oX, yi = oY;
         var xf = 500, yf = Math.round(500 * m);
 
         var dx = Math.abs(xf - xi), sx = xi < xf ? 1 : -1;
@@ -322,10 +382,8 @@ function highlight(x, y) {
         var err = (dx>dy ? dx : -dy)/2;
 
         while (true) {
-
             var idx = 4 * ((yOff + yi) * SIZE + xOff + xi);
             if ((xOff + xi >= SIZE) || data[idx] == 128) {
-                console.log("HIT!");
                 CTX.beginPath();
                 CTX.moveTo(xOff, yOff);
                 CTX.lineTo(xOff + xi, yOff + yi);
@@ -340,65 +398,35 @@ function highlight(x, y) {
             if (e2 < dy) { err += dx; yi += sy; }
         }
 
-        /* code graveyard
-        for (var x2 = 1; x2 < 500; x2 += 1) {
-            var y2 = m * x2;
 
-            var idx = 4 * ((yOff + Math.floor(y2)) * SIZE + xOff + Math.floor(x2));
-            if ((xOff + x2 >= SIZE || yOff + y2 >= SIZE) || (data[idx] == 128)) {
-                CTX.strokeStyle = color;
+        var xi = oX, yi = oY;
+        var xf = -500, yf = Math.round(-500 * m);
+
+        var dx = Math.abs(xf - xi), sx = xi < xf ? 1 : -1;
+        var dy = Math.abs(yf - yi), sy = yi < yf ? 1 : -1;
+        var err = (dx>dy ? dx : -dy)/2;
+
+        while (true) {
+            var idx = 4 * ((yOff + yi) * SIZE + xOff + xi);
+            if ((xOff + xi <= 0) || data[idx] == 128) {
                 CTX.beginPath();
                 CTX.moveTo(xOff, yOff);
-
-                CTX.lineTo(xOff + Math.floor(x2), yOff + Math.floor(y2));
+                CTX.lineTo(xOff + xi, yOff + yi);
                 CTX.stroke();
                 break;
             }
-        }*/
-        /*
-        for (var x2 = -1; x2 > -500; x2--) {
-            var y2 = m * x2;
 
-            var idx = 4 * ((yOff + Math.floor(y2)) * SIZE + xOff + Math.floor(x2));
-            //if ((xOff + x2 >= SIZE || yOff + y2 >= SIZE) || (data[idx] == 128)) {
-                console.log("ey");
-                CTX.beginPath();
-                CTX.moveTo(xOff, yOff);
 
-                CTX.lineTo(xOff + Math.floor(x2), yOff + Math.floor(y2));
-                CTX.stroke();
-                break;
-            //}
-        }*/
-        /*
-        for (var x2 = 1; x2 > -500; x2 -= 1){
-
-            var y2 = -m * x2;
-            /*
-            CTX.strokeStyle = "white";
-            CTX.beginPath();
-            CTX.moveTo(xOff, yOff);
-            CTX.lineTo(xOff + x2, yOff + y2);
-            CTX.stroke();
-            */
-            /*
-            var idx = 4 * ((yOff + Math.ceil(y2)) * SIZE + xOff + Math.ceil(x2));
-            if ((xOff + x2 >= SIZE || yOff + y2 >= SIZE) || (data[idx] == 128)) {
-                console.log("hit w/ offset ", x2, y2);
-                CTX.strokeStyle = color;
-                CTX.beginPath();
-                CTX.moveTo(xOff, yOff);
-
-                CTX.lineTo(xOff + Math.floor(x2), yOff + Math.floor(y2));
-                CTX.stroke();
-                break;
-            }
+            if (xi === xf && yi === yf) break;
+            var e2 = err;
+            if (e2 > -dx) { err -= dy; xi += sx; }
+            if (e2 < dy) { err += dx; yi += sy; }
         }
-        /*
-        x2 = -500;
-        y2 = m * x2;
-        console.log(y2);
-
-        */
     }
+
+    drawVertices();
+    redrawRects();
 }
+
+var maxNeeded = Math.floor(VERTICES.length / 3);
+document.getElementById("feedback-max").innerText = maxNeeded;
